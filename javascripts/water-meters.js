389c47e6a31ad6471b18fbb5394875cbdbb87820
huzferd <huzferd@gmail.com>
2016-09-23 23:54:25 +0300

    add submit function

--- javascripts/water-meters.js
+++ javascripts/water-meters.js
@@ -1,11 +1,29 @@
 ﻿$(function () {
     $("#water-form").validator();
     $("#water-form").on("submit",
-            function (event) {
-                if (event.isDefaultPrevented()) {
-                    alert("Error isDefaultPrevented!");
-                } else {
-                    event.preventDefault();
-                }
-            });
+        function (event) {
+            if (event.isDefaultPrevented()) {
+                alert("Ошибка! Заполните поля.");
+            } else {
+                event.preventDefault();
+                submitForm();
+            }
+        });
 });
+
+function submitForm() {
+    var postdata = $("#water-form").serialize();
+    $.ajax({
+        type: "POST",
+        url: "send-water-meters.php",
+        data: postdata,
+        success: function (text) {
+            if (text == "success") {
+                alert("Показания успешно отправлены");
+            } else {
+                alert("Во время отправки показаний, произошла ошибка");
+            }
+            location.reload();
+        }
+    });
+}
--- send-water-meters.php
+++ send-water-meters.php
@@ -1,6 +1,6 @@
 ﻿<?php
 
-$to      = "kollinz@bk.ru";
+$to      = "oooukzentvrmk@mail.ru";
 $subject = "Показания счетчиков воды";
 
 $headers = "MIME-Version: 1.0" . "\r\n";
@@ -8,21 +8,25 @@ $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 $headers .= "From: <water-meters@huzferd.ru>" . "\r\n";
 
 $body = "<h1>Показания счетчиков воды</h1>";
-$body .= "ФИО: ".$_POST["surname"]." ".$_POST["name"]." ".$_POST["middlename"]."</br>";
-$body .= "Адрес: ".$_POST["city"].", ".$_POST["street"].", д. ".$_POST["home"].", кв. ".$_POST["apartment"]."</br>";
+
 if (isset($_POST["settlementaccount"])) {
 	$body .= "Лицевой счет: ".$_POST["settlementaccount"]."</br>";
 }
-$body .= "Счетчик №1. Значение: ".$_POST["watermeterone"]." Тип водоснабжения: ".($_POST[""] ? "Горячее водоснабжение" : "Холодное водоснабжение")."</br>";
-$body .= "Счетчик №2. Значение: ".$_POST["watermetertwo"]." Тип водоснабжения: ".($_POST[""] ? "Горячее водоснабжение" : "Холодное водоснабжение")."</br>";
-if (isset($_POST["watermeterthree"])) {
-    $body .= "Счетчик №3. Значение: ".$_POST["watermeterthree"]." Тип водоснабжения: ".($_POST[""] ? "Горячее водоснабжение" : "Холодное водоснабжение")."</br>";
+
+$body .= "ФИО: ".$_POST["surname"]." ".$_POST["name"]." ".$_POST["middlename"]."</br>";
+$body .= "Адрес: ".$_POST["city"].", ".$_POST["street"].", д. ".$_POST["home"].", кв. ".$_POST["apartment"]."</br>";
+$body .= "<strong>Ванная комната:</strong>"."</br>";
+$body .= "Счетчик №1. ГВС Значение: ".$_POST["watermeterone"]."</br>";
+$body .= "Счетчик №2. ХВС Значение: ".$_POST["watermetertwo"]."</br>";
+
+if (isset($_POST["watermeterthree"]) && isset($_POST["watermeterfour"])) {
+    $body .= "<strong>Кухня:</strong>"."</br>";
+    $body .= "Счетчик №3. Значение: ".$_POST["watermeterthree"]."</br>";
+    $body .= "Счетчик №4. Значение: ".$_POST["watermeterfour"]."</br>";
 }
-if (isset($_POST["watermeterfour"])) {
-    $body .= "Счетчик №4. Значение: ".$_POST["watermeterfour"]." Тип водоснабжения: ".($_POST[""] ? "Горячее водоснабжение" : "Холодное водоснабжение")."</br>";
-}
+
 if(isset($_POST["message"])){
-	$body .= "Дополнительная информация: ".$_POST["message"]."</br>";
+	$body .= "</br>"."<strong>Дополнительная информация:</strong>"."</br>".$_POST["message"]."</br>";
 }
 
 $success = mail($to, $subject, $body, $headers);
