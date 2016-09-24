38e687d51a980ceb92f024d932e057e887949342
huzferd <huzferd@gmail.com>
2016-09-24 21:43:29 +0300

    add water type

--- send-water-meters.php
+++ send-water-meters.php
@@ -1,6 +1,6 @@
 ﻿<?php
 
-$to      = "oooukzentvrmk@mail.ru";
+$to      = "example@example.com";
 $subject = "Показания счетчиков воды";
 
 $headers = "MIME-Version: 1.0" . "\r\n";
@@ -10,23 +10,23 @@ $headers .= "From: <water-meters@huzferd.ru>" . "\r\n";
 $body = "<h1>Показания счетчиков воды</h1>";
 
 if (isset($_POST["settlementaccount"])) {
-	$body .= "Лицевой счет: ".$_POST["settlementaccount"]."</br>";
+	$body .= "<p><strong>Лицевой счет:</strong> ".$_POST["settlementaccount"]."</p>";
 }
 
-$body .= "ФИО: ".$_POST["surname"]." ".$_POST["name"]." ".$_POST["middlename"]."</br>";
-$body .= "Адрес: ".$_POST["city"].", ".$_POST["street"].", д. ".$_POST["home"].", кв. ".$_POST["apartment"]."</br>";
-$body .= "<strong>Ванная комната:</strong>"."</br>";
-$body .= "Счетчик №1. ГВС Значение: ".$_POST["watermeterone"]."</br>";
-$body .= "Счетчик №2. ХВС Значение: ".$_POST["watermetertwo"]."</br>";
+$body .= "<p>ФИО: ".$_POST["surname"]." ".$_POST["name"]." ".$_POST["middlename"]."</p>";
+$body .= "<p>Адрес: ".$_POST["city"].", ".$_POST["street"].", д. ".$_POST["home"].", кв. ".$_POST["apartment"]."</p>";
+$body .= "<p><strong>Ванная комната:</strong>"."</p>";
+$body .= "<p>Счетчик №1. ГВС Значение: ".$_POST["watermeterone"]."</p>";
+$body .= "<p>Счетчик №2. ХВС Значение: ".$_POST["watermetertwo"]."</p>";
 
 if (isset($_POST["watermeterthree"]) && isset($_POST["watermeterfour"])) {
-    $body .= "<strong>Кухня:</strong>"."</br>";
-    $body .= "Счетчик №3. Значение: ".$_POST["watermeterthree"]."</br>";
-    $body .= "Счетчик №4. Значение: ".$_POST["watermeterfour"]."</br>";
+    $body .= "<p><strong>Кухня:</strong>"."</p>";
+    $body .= "<p>Счетчик №3. ГВС Значение: ".$_POST["watermeterthree"]."</p>";
+    $body .= "<p>Счетчик №4. ХВС Значение: ".$_POST["watermeterfour"]."</p>";
 }
 
 if(isset($_POST["message"])){
-	$body .= "</br>"."<strong>Дополнительная информация:</strong>"."</br>".$_POST["message"]."</br>";
+	$body .= "<p>"."<strong>Дополнительная информация:</strong>"."</p><p>".$_POST["message"]."</p>";
 }
 
 $success = mail($to, $subject, $body, $headers);
